ba1b2cd7af8bfa3ed4afb079fb82506ccf77c704
Julie Edwards <julieedwards@Julies-MacBook-Pro.local>
2018-06-07 17:39:38 -0700

    var notes

---
+++ functions.js
@@ -0,0 +1,7 @@
+//ARROW FUNCTIONS
+
+const names = ['leo', 'rose', 'shiku'];
+
+const fullNames = names.map(function(name) {
+	
+})
---
+++ variables.js
@@ -0,0 +1,21 @@
+//VAR, LESS, CONST
+
+
+/*
+less/const
+	- cannot be re-initialized
+	- block scoped
+	- declaration is NOT hoisted (temporal dead zone)
+
+less
+	- works with setTimeout in for loops
+
+const
+	- cannot be re defined
+	- properties are mutable, however
+*/
+
+//quick block scope
+{
+	const name = 'julie';
+}
